0910d5d259fe8507d2060c8184c872a2ea8020e8
P3DRO1411 <102195393+P3DRO1411@users.noreply.github.com>
2022-04-10 13:54:40 -0300

    foi feito o jackson correndo

---
+++ sketch.js
@@ -0,0 +1,64 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+
+function preload(){
+pathImg = loadImage("path.png");
+boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+}
+
+function setup(){
+ createCanvas(400,400);
+// Movendo o fundo 
+ path=createSprites(200,200,10,10);
+ path.addImage("path",pathImg); 
+ path.velocityY= 4;
+ path.scale=1.2; 
+    
+
+//Criando menino que corre 
+boy=createSprite(200,200,10,10);
+boy.addImage("menino",boyImg);
+
+
+// Criando Boundary (Limite) esquerdo  
+leftBoundary=createSprite(0,0,100,800);
+leftBoundary.visible = false;
+
+
+//Crie Boundary direito 
+rightBoundary=createSprite(400,0,100,800);
+rightBoundary.visible=false;
+}
+
+
+function draw() {
+  
+  background(0);
+
+
+
+// Menino se movendo no eixo X com o mouse
+boy.x =World.mouseX;
+
+edges= createEdgeSprites();
+boy.collide(edges[3]);
+boy.collide(leftBoundary);
+boy.collide(rightBoundary);
+
+ //Reiniciar o fundo
+ if(path.x<0){
+  path.x=path.width/2;  
+    }
+
+
+
+ drawSprites();
+
+
+
+ }
+
+
+
